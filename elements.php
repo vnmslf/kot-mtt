2253c713cb0369ba161c4ff23f3f1b6f4aa9c818
miberezkin <vnmslf@gmail.com>
2024-05-17 16:11:59 +0300

    master: upd

---
+++ elements.php
@@ -0,0 +1,6 @@
+<?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');?>
+<div class="logo">
+	<img src="<?=SITE_TEMPLATE_PATH?>/images/logo.svg" alt="КОТ-МТТ, логотип" />
+	<span>Школа Покера</span>
+</div>
+<?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
--- local/templates/vector/components/bitrix/news/global__catalog/lang/ru/detail.php
+++
@@ -1,4 +0,0 @@
-<?
-$MESS ['T_NEWS_DETAIL_BACK'] = 'Вернуться в каталог';
-$MESS ['CATEGORIES'] = 'Материалы по теме:';
-?>
