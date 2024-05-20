786bee7888977ab88bb4b9bd2e487fdfc3ab0ce3
miberezkin <vnmslf@gmail.com>
2024-05-21 01:06:29 +0300

    master: order updates

--- local/templates/kot-mtt/inc/order.php
+++ local/templates/kot-mtt/inc/order.php
@@ -5,17 +5,19 @@ $modal_button_text = $arParams['MODAL_BUTTON_TEXT'];?>
 	<div class="form-section">
 		<div class="form-group">
 			<label>ФИО <span>*</span></label>
-			<input type="text" name="<?=$prefix?>-name" id="<?=$prefix?>-name" class="form-control form-control-lg required" value="" placeholder="Прим. Дмитрий" />
+			<input type="text" name="<?=$prefix?>-name" id="<?=$prefix?>-name" class="required" value="" placeholder="Прим. Дмитрий" />
 		</div>
 		<div class="form-group">
 			<label>Телефон <span>*</span></label>
-			<input type="tel" name="<?=$prefix?>-phone" id="<?=$prefix?>-phone" class="masked__phone form-control form-control-lg required" value="" placeholder="+7 (111) 111 11 11" maxlength="12" />
+			<input type="tel" name="<?=$prefix?>-phone" id="<?=$prefix?>-phone" class="masked__phone required" value="" placeholder="+7 (111) 111 11 11" maxlength="12" />
 		</div>
 		<div class="d-none">
 			<input type="text" id="<?=$prefix?>-botcheck" name="<?=$prefix?>-botcheck" value="">
 		</div>
 		<div class="form-submit">
 			<button type="submit" name="<?=$prefix?>-submit"><?=$modal_button_text?></button>
+			<input type="checkbox" name="checkbox" id="checkbox" class="required" value="" />
+			<label for="checkbox">Согласен на <a href="/">обработку персональных данных</a></label>
 		</div>
 		<input type="hidden" name="prefix" value="<?=$prefix?>-">
 		<input type="hidden" name="autoresponder" value="true">
