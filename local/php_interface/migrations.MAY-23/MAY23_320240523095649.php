a8c376ed8c5490b0a7699575e6912d569d4c6eeb
miberezkin <vnmslf@gmail.com>
2024-05-23 09:57:06 +0300

    master: MAY-23 iblock static

---
+++ local/php_interface/migrations.MAY-23/MAY23_320240523095649.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sprint\Migration;
+
+class MAY23_320240523095649 extends Version
+{
+    protected $description   = "";
+    protected $moduleVersion = "4.8.2";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setExchangeResource('iblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Iblock()
+                      ->addElement(
+                          $item['iblock_id'],
+                          $item['fields'],
+                          $item['properties']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function down()
+    {
+    }
+}
