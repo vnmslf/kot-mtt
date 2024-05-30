570c7fb6c5d867319bc7f805726f231d2ee43a42
miberezkin <vnmslf@gmail.com>
2024-05-30 17:48:31 +0300

    master: with fix 2

---
+++ local/php_interface/migrations.MAY-30/MAY30_920240530174806.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class MAY30_920240530174806 extends Version
+{
+    protected $description = "";
+
+    protected $moduleVersion = "4.8.2";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+
+        $iblockId = $helper->Iblock()->getIblockIdIfExists(
+            'with',
+            'Static'
+        );
+
+        $helper->Iblock()->addSectionsFromTree(
+            $iblockId,
+            array (
+)        );
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
